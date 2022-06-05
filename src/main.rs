c1611c8afe039a78a7775d1a7e6a44c8f9db4a39
Daniel Xu <dxu@dxuuu.xyz>
2022-06-05 18:33:12 -0500

    Add DB support

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,20 @@
-use std::path::{Path, PathBuf};
+use std::path::PathBuf;
 
 use anyhow::Result;
 use clap::Parser;
-use log::{info, LevelFilter};
-use rusqlite::{Connection, NO_PARAMS};
+use log::{error, info, LevelFilter};
+use rand::Rng;
+use rusqlite::{Connection, OptionalExtension};
 use simple_logger::SimpleLogger;
-use tide::{Request, StatusCode};
+use tide::{utils::After, Request, Response, StatusCode};
+
+const CHARSET: &[u8] = b"abcdefghijklmnopqrstuvwxyz0123456789";
 
 #[derive(Parser, Debug)]
 struct Args {
     #[structopt(short, long, default_value = "3400")]
     port: u16,
-    #[structopt(long, default_value = "/etc/pastes.sqlite3", parse(from_os_str))]
+    #[structopt(long, default_value = "./pastes.sqlite3", parse(from_os_str))]
     db: PathBuf,
 }
 
@@ -29,18 +32,30 @@ impl State {
     }
 }
 
-fn get_connection(path: &Path) -> Result<Connection> {
-    let conn = Connection::open(path)?;
+fn get_connection(state: &State) -> Result<Connection> {
+    let conn = Connection::open(&state.db)?;
 
     conn.execute(
-        "create table if not exists pastes (
-             id string primary key unique,
-             contents text not null
+        "CREATE TABLE IF NOT EXISTS pastes (
+             id STRING PRIMARY KEY UNIQUE,
+             contents TEXT NOT NULL
          )",
-        NO_PARAMS,
+        [],
     )?;
 
     Ok(conn)
+}
+
+/// Generates a random id
+fn gen_id() -> String {
+    let mut rng = rand::thread_rng();
+
+    (0..6)
+        .map(|_| {
+            let idx = rng.gen_range(0..CHARSET.len());
+            CHARSET[idx] as char
+        })
+        .collect()
 }
 
 async fn homepage(mut _req: Request<State>) -> tide::Result {
@@ -50,16 +65,38 @@ async fn homepage(mut _req: Request<State>) -> tide::Result {
 
 async fn get(req: Request<State>) -> tide::Result {
     let id = req.param("id")?;
-    info!("get id={}", id);
+    info!("GET id={}", id);
 
-    Ok(StatusCode::Ok.into())
+    let conn = get_connection(req.state())?;
+    let contents: Option<String> = conn
+        .query_row(
+            r#"SELECT contents FROM pastes WHERE id=(?1)"#,
+            [id],
+            |row| row.get(0),
+        )
+        .optional()?;
+
+    match contents {
+        Some(c) => Ok(c.into()),
+        None => Ok(StatusCode::NotFound.into()),
+    }
 }
 
 async fn post(mut req: Request<State>) -> tide::Result {
     let body = req.body_string().await?;
-    info!("post: {} bytes", body.len());
+    info!("POST: {} bytes", body.len());
 
-    Ok(StatusCode::Created.into())
+    let id = gen_id();
+    let conn = get_connection(req.state())?;
+    conn.execute(
+        "INSERT INTO pastes (id, contents) VALUES (?1, ?2)",
+        [&id, &body],
+    )?;
+
+    let mut resp = Response::new(StatusCode::Created);
+    resp.set_body(format!("https://pastes.dxuuu.xyz/{id}\n"));
+
+    Ok(resp)
 }
 
 #[async_std::main]
@@ -73,6 +110,12 @@ async fn main() -> tide::Result<()> {
         .unwrap();
 
     let mut app = tide::with_state(State::new(&args)?);
+    app.with(After(|res: Response| async {
+        if let Some(err) = res.error() {
+            error!("Internal error: {err}");
+        }
+        Ok(res)
+    }));
     app.at("/").get(homepage);
     app.at("/:id").get(get);
     app.at("/").post(post);
